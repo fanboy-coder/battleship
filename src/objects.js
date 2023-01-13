20b5b7e1590f082872bd2f456832a2cb537b83fe
Fanboy Coder <joel.monteiro.ferreira@gmail.com>
2023-01-13 18:14:21 +0000

    Finished the horizontal ship placement

--- src/objects.js
+++ src/objects.js
@@ -18,43 +18,57 @@ class Ship {
 class Gameboard {
 	constructor() {
 		this.board = [];
-		const columns = ["A","B","C","D","E","F","G","H","I","J"];
 
-		function letterPositions(firstNum, result =[]){
-			if(result.length === vessel.length) return;
-			result.push(firstLetter+firstNum);
-			letterPositions(firstNum+1, result);
-		}
+		const columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
-		function numberPositions(letter, firstNum, result = []) {
-			if(result.length === vessel.length) return;
-			let correctLetter = columns[letter];
-			result.push(correctLetter+=firstNum);
-			numberPositions(letter+1,firstNum, result);
-		}
-		
 		this.newBoard = function () {
-			columns.forEach(column => {				
-				for(let i=1; i<11; i++) {
-					this.board.push(column+i);
+			columns.forEach(column => {
+				for (let i = 1; i < 11; i++) {
+					this.board.push(column + i);
 				};
 			});
 		};
 
-		this.placeShip = function (ship,xPos,yPos) {
+		this.placeShip = function (ship, xPos, yPos) {
 			const vessel = new Ship(ship);
-			let firstNum = xPos.slice(1,3);
-			let secondNum = yPos.slice(1,3);
-			let firstLetter = xPos.slice(0,1);
-			let secondLetter = yPos.slice(0,1);
+			let firstNum = xPos.slice(1, 3);
+			let secondNum = yPos.slice(1, 3);
+			let firstLetter = xPos.slice(0, 1);
+			let secondLetter = yPos.slice(0, 1);
 
-			if ((firstNum - secondNum) === -vessel.length + 1 || ((columns.indexOf(secondLetter)+1)-columns.indexOf(firstLetter)) === vessel.length) {
+			let result = [];
+			let board = this.board;
+
+			function letterPositions(firstLetter, firstNum) {
+				if (result.length === vessel.length) return;
+				result.push(firstLetter + firstNum);
+				letterPositions(firstLetter, firstNum + 1);
+			};
+
+			function numberPositions(letter, firstNum) {
+				if (result.length === vessel.length) return;
+				result.push(columns[letter] += firstNum);
+				numberPositions(letter + 1, firstNum);
+			};
+
+			function removeSpaces() {
+				result.forEach(entry => {
+					let place = board.indexOf(entry);
+					board.splice(place, 1);
+				});
+			};
+
+			if ((firstNum - secondNum) === -vessel.length + 1 || ((columns.indexOf(secondLetter) + 1) - columns.indexOf(firstLetter)) === vessel.length) {
 				if (firstLetter === secondLetter) {
-					letterPositions(firstNum);
+					result.length = 0;
+					letterPositions(firstLetter, firstNum);
+					removeSpaces();
 				}
-				else if(firstNum === secondNum) {
+				else if (firstNum === secondNum) {
 					let letter = columns.indexOf(firstLetter);
-					numberPositions(letter,firstNum);
+					result.length = 0;
+					numberPositions(letter, firstNum);
+					removeSpaces();
 				}
 				else {
 					console.log("Invalid position");
@@ -63,9 +77,9 @@ class Gameboard {
 			else {
 				console.log("The spaces you selected do not match your ship's size.")
 			};
-	}
-}
-}
+		};
+	};
+};
 
 
-module.exports = {Ship, Gameboard};
+module.exports = { Ship, Gameboard };
